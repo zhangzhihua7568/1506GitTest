88ce67a2dac226b9062ac3c0350fcc219dff77ea
tarean <tarean@tareandeiMac.local>
2015-10-08 11:55:58 +0800

    提交为了

---
+++ Demo02-GitServer/Demo02-GitServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo02-GitServer
+//
+//  Created by tarean on 15/10/8.
+//  Copyright (c) 2015年 tarean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
